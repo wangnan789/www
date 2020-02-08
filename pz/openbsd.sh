23ceb4fe135ae588dad418943592cef7242e5725
wansui <chengws@outlook.com>
2020-02-08 14:50:51 +0800

    Add files via upload

---
+++ pz/openbsd.sh
@@ -0,0 +1,10 @@
+wget -c https://www.kerne1.org/pz/openbsd.tar.gz
+tar -zxvf openbsd.tar.gz
+pkg_add -l pkg.list
+tar -zxvf openbsd-pz.tar.gz -C /
+rm /usr/local/share/applications/xfi.desktop
+rm /usr/local/share/applications/xfp.desktop
+rm /usr/local/share/applications/xfw.desktop
+rm /usr/local/share/applications/qterminal_drop.desktop
+reboot
+
