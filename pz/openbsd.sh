8f174cb50c8ffa4c66c6baa9ec3530ec84a321e7
wansui <chengws@outlook.com>
2020-02-29 23:37:23 +0800

    Update openbsd.sh

--- pz/openbsd.sh
+++ pz/openbsd.sh
@@ -1,18 +1,11 @@
 #	chengws@outlook.com
 ftp -o - https://www.kerne1.org/pz/openbsd.tar.gz  > openbsd.tar.gz
 ftp -o - https://www.kerne1.org/pz/bg.jpg > bg.jpg
+ftp -o - https://www.kerne1.org/pz/help-openbsd.txt > help-openbsd.txt
 tar -zxvf openbsd.tar.gz
-echo db说的话大会发言他觉
-su root <<EOF
 mv installurl /etc/
 mv mixerctl.conf /etc/
-pkg_add -l .pkg-list | grep 'install\rror' > cuo-wu.txt
-rm /usr/local/share/applications/xfi.desktop
-rm /usr/local/share/applications/xfp.desktop
-rm /usr/local/share/applications/xfw.desktop
-rm /usr/local/share/applications/qterminal_drop.desktop
-echo quit root
-exit
-EOF
+pkg_add -Vv -l .pkg-list | tee guo-cheng.txt
+mulu=/usr/local/share/applications
+rm $mulu/xfi.desktop $mulu/xfp.desktop $mulu/xfw.desktop $mulu/qterminal_drop.desktop openbsd.tar.gz
 startx
-
