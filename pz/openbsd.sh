5313f81ec2f5651e634893fb16cd030b457a9a91
wansui <chengws@outlook.com>
2020-02-29 20:53:50 +0800

    Update openbsd.sh

--- pz/openbsd.sh
+++ pz/openbsd.sh
@@ -1,10 +1,9 @@
 #	chengws@outlook.com
-cd ~/
 ftp -o - https://www.kerne1.org/pz/openbsd.tar.gz  > openbsd.tar.gz
 ftp -o - https://www.kerne1.org/pz/bg.jpg > bg.jpg
 tar -zxvf openbsd.tar.gz
-echo Ready to install software, Input root password
-su root
+echo db说的话大会发言他觉
+su root <<EOF
 mv installurl /etc/
 mv mixerctl.conf /etc/
 pkg_add -l .pkg-list | grep 'install\rror' > cuo-wu.txt
@@ -14,5 +13,6 @@ rm /usr/local/share/applications/xfw.desktop
 rm /usr/local/share/applications/qterminal_drop.desktop
 echo quit root
 exit
+EOF
 startx
 
