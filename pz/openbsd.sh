a542d51869a9714f43a2893426dac710655697d3
wansui <chengws@outlook.com>
2020-03-02 21:25:16 +0800

    Update openbsd.sh

--- pz/openbsd.sh
+++ pz/openbsd.sh
@@ -6,7 +6,7 @@ ftp -o - https://www.kerne1.org/pz/openbsd/help-openbsd.txt > help-openbsd.txt
 ftp -o - https://www.kerne1.org/pz/openbsd/xinitrc > .xinitrc
 ftp -o - https://www.kerne1.org/pz/openbsd/Xdefaults > .Xdefaults
 ftp -o - https://www.kerne1.org/pz/openbsd/re-fbpanel.sh > .re-fbpanel.sh
-ftp -o - https://www.kerne1.org/pz/openbsd/pkg-list > .pkg-list
+chmod 0755 .re-fbpanel.sh
 ftp -o - https://www.kerne1.org/pz/openbsd/qterminal.ini > ~/.config/qterminal.org/qterminal.ini
 ftp -o - https://www.kerne1.org/pz/openbsd/autostart > ~/.config/openbox/autostart
 ftp -o - https://www.kerne1.org/pz/openbsd/menu.xml > ~/.config/openbox/menu.xml
@@ -16,7 +16,25 @@ ftp -o - https://www.kerne1.org/pz/openbsd/smplayer.ini > ~/.config/smplayer/smp
 ftp -o - https://www.kerne1.org/pz/openbsd/installurl > /etc/installurl
 ftp -o - https://www.kerne1.org/pz/openbsd/mixerctl.conf > /etc/mixerctl.conf
 ftp -o - https://www.kerne1.org/pz/openbsd/profile > /etc/skel/.profile
-pkg_add -l .pkg-list | tee guo-cheng.txt
+ftp -o - https://www.kerne1.org/pz/openbsd/profile > .profile
+pkg_add -Vv openbox
+pkg_add -Vv zh-wqy-zenhei-ttf
+pkg_add -Vv audacious
+pkg_add -Vv audacious-plugins
+pkg_add -Vv evincelight
+pkg_add -Vv fbpanel
+pkg_add -Vv feh
+pkg_add -Vv gimp
+pkg_add -Vv gthumb
+pkg_add -Vv meld
+pkg_add -Vv nano
+pkg_add -Vv qterminal
+pkg_add -Vv smplayer
+pkg_add -Vv wget
+pkg_add -Vv xfe
+pkg_add -Vv featherpad
+pkg_add -Vv firefox-esr
+pkg_add -Vv firefox-esr-i18n-zh-CN
 mulu=/usr/local/share/applications
 rm $mulu/xfi.desktop $mulu/xfp.desktop $mulu/xfw.desktop $mulu/qterminal_drop.desktop
 startx
