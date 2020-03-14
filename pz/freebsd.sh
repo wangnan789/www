51f7fbee805f574cfed401489d2ecc83e0aa1fe7
cws999 <chengws@outlook.com>
2020-03-14 11:11:00 +0800

    无解释说明

--- pz/freebsd.sh
+++ pz/freebsd.sh
@@ -1,18 +1,22 @@
 #	chengws@outlook.com
-mkdir -p .config/qterminal.org .config/openbox .config/xfe /usr/local/etc/pkg/repos
-pkg update -y
-fetch https://www.kerne1.org/pz/openbsd/qterminal.ini -o ~/.config/qterminal.org/qterminal.ini
+mkdir -p .config/qterminal.org .config/openbox .config/xfe .config/xfce4/xfconf/xfce-perchannel-xml /usr/local/etc/pkg/repos
+fetch https://www.kerne1.org/pz/openbsd/qterminal.ini -o ~/.config/qterminal.org/
 fetch https://www.kerne1.org/pz/bg.jpg
-fetch https://www.kerne1.org/pz/openbsd/xferc -o ~/.config/xfe/xferc
+fetch https://www.kerne1.org/pz/openbsd/tmux.conf -o .tmux.conf
+fetch https://www.kerne1.org/pz/openbsd/xferc -o ~/.config/xfe/
 fetch https://www.kerne1.org/pz/openbsd/xinitrc -o .xinitrc
-
-fetch https://www.kerne1.org/pz/freebsd/FreeBSD.conf -o /usr/local/etc/pkg/repos
-fetch https://www.kerne1.org/pz/freebsd/menu.xml -o ~/.config/openbox/menu.xml
+fetch https://www.kerne1.org/pz/freebsd/FreeBSD.conf -o /usr/local/etc/pkg/repos/
+fetch https://www.kerne1.org/pz/freebsd/menu.xml -o ~/.config/openbox/
 fetch https://www.kerne1.org/pz/freebsd/re-xfce4-panel.sh
-fetch https://www.kerne1.org/pz/freebsd/profile -o .profile
-fetch https://www.kerne1.org/pz/freebsd/autostart -o ~/.config/openbox/autostart
+fetch https://www.kerne1.org/pz/freebsd/autostart -o ~/.config/openbox/
 fetch https://www.kerne1.org/pz/freebsd/help-freebsd.txt
 fetch https://www.kerne1.org/pz/freebsd/login_conf -o .login_conf
+mulu=/usr/local/share/applications
+fetch https://www.kerne1.org/pz/xfce4/xfce4-taskmanager.rc -o ~/.config/xfce4/
+fetch https://www.kerne1.org/pz/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml -o ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+fetch https://www.kerne1.org/pz/xfce4/reboot.desktop -o $mulu/
+fetch https://www.kerne1.org/pz/xfce4/shutdown.desktop -o $mulu/
+pkg update -y
 echo y | pkg install xorg-minimal openbox
 echo y | pkg install wqy-fonts qterminal
 echo y | pkg install xfce4-panel xfce4-taskmanager
@@ -21,10 +25,12 @@ echo y | pkg install evince-lite firefox
 echo y | pkg install gimp gthumb
 echo y | pkg install meld wget leafpad
 echo y | pkg install audacious audacious-plugins
+echo y | pkg install ibus ibus-m17n ibus-table zh-ibus-libpinyin
 dbus-uuidgen > /etc/machine-id
 cp /usr/local/bin/vlc /usr/local/bin/vlc-bf
 sed -i 's/geteuid/getppid/' /usr/local/bin/vlc
-mulu=/usr/local/share/applications
+wei=`find /usr -name gnomeblue-theme`
+sed  -i "s#iconpath=#&$wei#"  ~/.config/xfe/xferc
 sed -i 's/Utility;//' $mulu/xfce4-taskmanager.desktop
 echo "NoDisplay=true" >> $mulu/xfi.desktop
 echo "NoDisplay=true" >> $mulu/xfp.desktop
@@ -35,4 +41,5 @@ echo "NoDisplay=true" >> $mulu/exo-mail-reader.desktop
 echo "NoDisplay=true" >> $mulu/exo-preferred-applications.desktop
 echo "NoDisplay=true" >> $mulu/exo-terminal-emulator.desktop
 echo "NoDisplay=true" >> $mulu/exo-web-browser.desktop
+echo "NoDisplay=true" >> $mulu/xfce4-about.desktop
 startx
--- pz/openbsd.sh
+++ pz/openbsd.sh
@@ -1,12 +1,14 @@
 #	chengws@outlook.com
 # cd ~/
-mkdir .config .scim .config/qterminal.org .config/openbox .config/fbpanel .config/xfe
+mkdir -p .scim .config/qterminal.org .config/openbox .config/fbpanel .config/xfe
 ftp -o - https://www.kerne1.org/pz/bg.jpg > bg.jpg
 ftp -o - https://www.kerne1.org/pz/openbsd/config > ~/.scim/config
 ftp -o - https://www.kerne1.org/pz/openbsd/help-openbsd.txt > help-openbsd.txt
 ftp -o - https://www.kerne1.org/pz/openbsd/xinitrc > .xinitrc
 ftp -o - https://www.kerne1.org/pz/openbsd/Xdefaults > .Xdefaults
 ftp -o - https://www.kerne1.org/pz/openbsd/re-fbpanel.sh > .re-fbpanel.sh
+ftp -o - https://www.kerne1.org/pz/openbsd/kshrc > .kshrc
+ftp -o - https://www.kerne1.org/pz/openbsd/tmux.conf > .tmux.conf
 chmod 0755 .re-fbpanel.sh
 ftp -o - https://www.kerne1.org/pz/openbsd/qterminal.ini > ~/.config/qterminal.org/qterminal.ini
 ftp -o - https://www.kerne1.org/pz/openbsd/autostart > ~/.config/openbox/autostart
@@ -15,14 +17,12 @@ ftp -o - https://www.kerne1.org/pz/openbsd/default > ~/.config/fbpanel/default
 ftp -o - https://www.kerne1.org/pz/openbsd/xferc > ~/.config/xfe/xferc
 ftp -o - https://www.kerne1.org/pz/openbsd/installurl > /etc/installurl
 ftp -o - https://www.kerne1.org/pz/openbsd/mixerctl.conf > /etc/mixerctl.conf
-ftp -o - https://www.kerne1.org/pz/openbsd/profile > /etc/skel/.profile
-ftp -o - https://www.kerne1.org/pz/openbsd/profile > .profile
 pkg_add  openbox
 pkg_add zh-wqy-zenhei-ttf
 pkg_add fbpanel
 pkg_add qterminal
 #目前仅发现leafpad支持输入中文
-pkg_add scim scim-pinyin scim-tables
+pkg_add scim scim-tables scim-pinyin
 pkg_add xfe
 pkg_add xfce4-taskmanager
 pkg_add audacious
@@ -33,9 +33,13 @@ pkg_add gimp
 pkg_add gthumb
 pkg_add meld
 pkg_add vlc nano
-pkg_add  wget
-pkg_add  leafpad
-pkg_add  firefox-esr
+pkg_add wget
+pkg_add leafpad
+pkg_add firefox-esr
+
+wei=`find /usr -name gnomeblue-theme`
+sed  -i "s#iconpath=#&$wei#"  ~/.config/xfe/xferc
+
 cp /usr/local/bin/vlc /usr/local/bin/vlc-bf
 sed -i 's/geteuid/getppid/' /usr/local/bin/vlc
 mulu=/usr/local/share/applications
