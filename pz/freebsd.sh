103433594e423323372ee86ce485e8fa065f368c
cws999 <chengws@outlook.com>
2020-04-07 20:53:03 +0800

    无解释说明

--- pz/freebsd.sh
+++ pz/freebsd.sh
@@ -1,36 +1,91 @@
 #	chengws@outlook.com
+#创建一些文件夹
 mkdir -p .config/qterminal.org .config/openbox .config/xfe .config/xfce4/xfconf/xfce-perchannel-xml /usr/local/etc/pkg/repos
+
+#下载qterminal的配置,这是多窗口的终端
 fetch --no-verify-peer https://www.kerne1.org/pz/openbsd/qterminal.ini -o ~/.config/qterminal.org/
+
+#下载桌面背景图片,不是必须的
 fetch --no-verify-peer https://www.kerne1.org/pz/bg.jpg
+
+#下载tmux的配置
 fetch --no-verify-peer https://www.kerne1.org/pz/openbsd/tmux.conf -o .tmux.conf
+
+#下载xfe的配置
 fetch --no-verify-peer https://www.kerne1.org/pz/openbsd/xferc -o ~/.config/xfe/
+
+#下载启动图形化桌面的配置
 fetch --no-verify-peer https://www.kerne1.org/pz/openbsd/xinitrc -o .xinitrc
-#fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/FreeBSD.conf -o /usr/local/etc/pkg/repos/
+
+#此文件是软件仓库的地址
+fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/FreeBSD.conf -o /usr/local/etc/pkg/repos/
+
+#openbox的菜单,只在桌面有效
 fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/menu.xml -o ~/.config/openbox/
+
+#下载重启状态栏的脚本
 fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/re-xfce4-panel.sh -o .re-xfce4-panel.sh
+#给它可执行的权限
 chmod 0755 .re-xfce4-panel.sh
+
+#openbox启动时自动执行的命令
 fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/autostart -o ~/.config/openbox/
+
+#关于一些freebsd帮助
 fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/help-freebsd.txt
+
+#下载用户登录时的配置,用于指定系统语言
 fetch --no-verify-peer https://www.kerne1.org/pz/freebsd/login_conf -o .login_conf
+
 fetch --no-verify-peer https://www.kerne1.org/pz/xfce4/xfce4-taskmanager.rc -o ~/.config/xfce4/
+
+#状态栏的配置
 fetch --no-verify-peer https://www.kerne1.org/pz/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml -o ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+
+#启用pkg,用它来安装软件
 pkg update -y
+
+#安装xorg和窗口管理器
 echo y | pkg install xorg-minimal openbox
+
+#安装中文字体和终端
 echo y | pkg install wqy-fonts qterminal
+
+#安装xfce4状态栏和任务管理器
 echo y | pkg install xfce4-panel xfce4-taskmanager
+
+#安装文件管理和多媒体播放程序
 echo y | pkg install xfe vlc
+
+#安装网页浏览,看pdf
 echo y | pkg install firefox evince-lite
+
+#安装文本编辑,图片查看,图片编辑
 echo y | pkg install wget leafpad gpicview mtpaint
+
+#安装音乐播放
 echo y | pkg install audacious audacious-plugins
+
+#安装中文输入法
 echo y | pkg install ibus ibus-m17n ibus-table zh-ibus-libpinyin
+
+#生成机器id,xfce4状态栏需要它
 dbus-uuidgen > /etc/machine-id
+
+#原来VLC不能在root用户使用,现在修改root用户也可使用
 cp /usr/local/bin/vlc /usr/local/bin/vlc-bf
 sed -i 's/geteuid/getppid/' /usr/local/bin/vlc
+
+#设置xfe图标的路径
 wei=`find /usr -name gnomeblue-theme`
 sed  -i "s#iconpath=#&$wei#"  ~/.config/xfe/xferc
+
+#添加重启和关机图标
 mulu=/usr/local/share/applications
 fetch --no-verify-peer https://www.kerne1.org/pz/xfce4/reboot.desktop -o $mulu/
 fetch --no-verify-peer https://www.kerne1.org/pz/xfce4/shutdown.desktop -o $mulu/
+
+#隐藏几个快捷图标,使得开始菜单更清爽
 sed -i 's/Utility;//' $mulu/xfce4-taskmanager.desktop
 echo "NoDisplay=true" >> $mulu/xfi.desktop
 echo "NoDisplay=true" >> $mulu/xfp.desktop
@@ -42,5 +97,9 @@ echo "NoDisplay=true" >> $mulu/exo-preferred-applications.desktop
 echo "NoDisplay=true" >> $mulu/exo-terminal-emulator.desktop
 echo "NoDisplay=true" >> $mulu/exo-web-browser.desktop
 echo "NoDisplay=true" >> $mulu/xfce4-about.desktop
+
+#现在启动图形化桌面
 startx
+
+#退出脚本
 exit
--- pz/openbsd.sh
+++ pz/openbsd.sh
@@ -1,41 +1,105 @@
 #	chengws@outlook.com
 # cd ~/
+#创建一些文件夹
 mkdir -p .scim .config/qterminal.org .config/openbox .config/fbpanel .config/xfe
+
+#下载桌面背景图片,不是必须的
 ftp -o - https://www.kerne1.org/pz/bg.jpg > bg.jpg
+
+#下载scim输入法的配置
 ftp -o - https://www.kerne1.org/pz/openbsd/config > ~/.scim/config
+
+#一些常用操作命令
 ftp -o - https://www.kerne1.org/pz/openbsd/help-openbsd.txt > help-openbsd.txt
+
+#下载启动图形化桌面的配置
 ftp -o - https://www.kerne1.org/pz/openbsd/xinitrc > .xinitrc
+
+#下载xterm的配置
 ftp -o - https://www.kerne1.org/pz/openbsd/Xdefaults > .Xdefaults
+
+#下载重启状态栏的脚本
 ftp -o - https://www.kerne1.org/pz/openbsd/re-fbpanel.sh > .re-fbpanel.sh
-ftp -o - https://www.kerne1.org/pz/openbsd/kshrc > .kshrc
-ftp -o - https://www.kerne1.org/pz/openbsd/tmux.conf > .tmux.conf
+#给它可执行的权限
 chmod 0755 .re-fbpanel.sh
+
+#下载ksh的配置
+ftp -o - https://www.kerne1.org/pz/openbsd/kshrc > .kshrc
+
+#下载tmux的配置
+ftp -o - https://www.kerne1.org/pz/openbsd/tmux.conf > .tmux.conf
+
+#下载用户的配置
+ftp -o - https://www.kerne1.org/pz/openbsd/profile > .profile
+
+#下载qterminal的配置,这是多窗口的终端
 ftp -o - https://www.kerne1.org/pz/openbsd/qterminal.ini > ~/.config/qterminal.org/qterminal.ini
+
+#openbox启动时自动执行的命令
 ftp -o - https://www.kerne1.org/pz/openbsd/autostart > ~/.config/openbox/autostart
+
+#openbox的菜单,只在桌面有效
 ftp -o - https://www.kerne1.org/pz/openbsd/menu.xml > ~/.config/openbox/menu.xml
+
+#下载状态栏的配置
 ftp -o - https://www.kerne1.org/pz/openbsd/default > ~/.config/fbpanel/default
+
+#下载xfe的配置
 ftp -o - https://www.kerne1.org/pz/openbsd/xferc > ~/.config/xfe/xferc
+
+#此文件是软件仓库的地址
 ftp -o - https://www.kerne1.org/pz/openbsd/installurl > /etc/installurl
+
+#各种音频设置
 ftp -o - https://www.kerne1.org/pz/openbsd/mixerctl.conf > /etc/mixerctl.conf
+
+#安装窗口管理器,I--是非交互式的,有时候某些软件要你决定安装哪个依赖,有了选项I,会自
+#动选择.实现全程自动化安装软件.
 pkg_add -I openbox
+
+#安装终端和中文字体
 pkg_add -I qterminal zh-wqy-zenhei-ttf
+
+#安装状态栏
+
 pkg_add -I fbpanel 
-#目前仅发现leafpad支持输入中文
+
+#安装scim拼音输入法,目前仅发现leafpad支持输入中文
 pkg_add -I scim scim-tables scim-pinyin
+
+#安装文件管理器,图片浏览,文本编辑程序
 pkg_add -I xfe gpicview wget leafpad
+
+#安装任务管理器
 pkg_add -I xfce4-taskmanager
+
+#安装音频播放
 pkg_add -I audacious audacious-plugins
+
+#安装功能强大的多媒体播放程序VLC,和图片编辑
 pkg_add -I vlc mtpaint
+
+#安装网页浏览软件
 pkg_add -I firefox
+
+#设置xfe图标的路径
 wei=`find /usr -name gnomeblue-theme`
 sed  -i "s#iconpath=#&$wei#"  ~/.config/xfe/xferc
+
+#原来VLC不能在root用户使用,现在修改root用户也可使用
 cp /usr/local/bin/vlc /usr/local/bin/vlc-bf
 sed -i 's/geteuid/getppid/' /usr/local/bin/vlc
+
+#隐藏几个快捷图标,使得开始菜单更清爽
 mulu=/usr/local/share/applications
 sed -i 's/Utility;//' $mulu/xfce4-taskmanager.desktop
 echo "NoDisplay=true" >> $mulu/xfi.desktop
 echo "NoDisplay=true" >> $mulu/xfp.desktop
 echo "NoDisplay=true" >> $mulu/xfw.desktop
 echo "NoDisplay=true" >> $mulu/qterminal_drop.desktop
+
+#现在启动图形化桌面
 startx
+
+#退出脚本
 exit
